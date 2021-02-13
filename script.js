a6287e7d5d3caf3646a167280222a836d8f118a6
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-02-13 18:06:20 -0500

    Table Data may now be clicked

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ function createTable(table){
     for(col=0;col<10;col++){
       var tableData = document.createElement('td');
       var data = document.createTextNode("X");
-      tableData.appendChild(data);
-      // TODO add onlick feature to each tableData var
-      tableRow.appendChild(tableData);
+      tableData.appendChild(data);      tableRow.appendChild(tableData);
+      tableData.onclick = function(){clickedTableData(this);};
     }
-    table.appendChild(tableRow);
+    table.appendChild(tableRow); 
   }
 }
 function clickedTableData(){
-  
+  console.log("It works");
 }
 
 table = document.createElement('table'); // Create table element
+table.setAttribute("onmousedown", "clickedTableData"); // So able elements are clickable
 createTable(table);
 document.body.appendChild(table); // Append table element to body
